dde4069eb484fc1fa7a97844ba3fcc1c31577976
Wind <feng19105306350@163.com>
2023-01-03 23:40:07 +0800

    mod: update eslintrc

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-    browser: true,
-  },
-  extends: ['eslint:recommended', 'plugin:vue/recommended', 'plugin:prettier/recommended'],
-}
